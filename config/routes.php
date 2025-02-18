c9b314604601b664f9f04ae19e1c70312aac413c
jihenJN <jihenjihen23@gmail.com>
2025-02-18 15:24:07 +0100

    feat : add bootsrap and the menu and modify home page to visite page

--- config/routes.php
+++ config/routes.php
@@ -55,7 +55,9 @@ return function (RouteBuilder $routes): void {
          * its action called 'display', and we pass a param to select the view file
          * to use (in this case, templates/Pages/home.php)...
          */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+       
+         // Redirect '/' to 'VisitesController@index'
+        $builder->connect('/', ['controller' => 'Visites', 'action' => 'index']);
 
         /*
          * ...and connect the rest of 'Pages' controller's URLs.
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -29,11 +29,14 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 
     <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
 
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+ 
+
+
 
 
     <?= $this->fetch('meta') ?>
@@ -41,15 +44,42 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
+    <!--nav class="top-nav">
         <div class="top-nav-title">
             <a href="<?= $this->Url->build('/') ?>"><span>Visites</span>Techniques</a>
         </div>
-        <!--div class="top-nav-links">
+        <div class="top-nav-links">
             <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
             <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div-->
-    </nav>
+        </div>
+    </nav-->
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand fs-3" href="<?= $this->Url->build('/') ?>">Gestion Visites Technique</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto fs-4">
+        <li class="nav-item">
+          <?= $this->Html->link('Visites', ['controller' => 'Visites', 'action' => 'index'], ['class' => 'nav-link']) ?>
+        </li>
+        <li class="nav-item">
+          <?= $this->Html->link('Clients', ['controller' => 'Clients', 'action' => 'index'], ['class' => 'nav-link']) ?>
+        </li>
+        <li class="nav-item">
+          <?= $this->Html->link('Visiteurs', ['controller' => 'Visiteurs', 'action' => 'index'], ['class' => 'nav-link']) ?>
+        </li>
+        <li class="nav-item">
+          <?= $this->Html->link('TypeContacts', ['controller' => 'TypeContacts', 'action' => 'index'], ['class' => 'nav-link']) ?>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
     <main class="main">
         <div class="container">
             <?= $this->Flash->render() ?>
