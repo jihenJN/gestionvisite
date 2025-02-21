7a5afc104f3379f8740e71c7d17926cc3114a8ee
jihenJN <jihenjihen23@gmail.com>
2025-02-21 15:12:11 +0100

    fix

--- templates/Visites/add.php
+++ templates/Visites/add.php
@@ -25,8 +25,8 @@
                     echo $this->Form->control('commentaire');
                     echo $this->Form->control('lieu');
                     echo $this->Form->control('date_demande', ['empty' => true]);
-                    echo $this->Form->control('date_prevu', ['empty' => true]);
-                    echo $this->Form->control('date_visite', ['empty' => true]); // Restricts future dates
+                    echo $this->Form->control('date_prevu', ['empty' => true ,'min'=> date('Y-m-d')]);
+                    echo $this->Form->control('date_visite', ['empty' => true ,'max'=> date('Y-m-d')]); // Restricts future dates
                     echo $this->Form->control('localisation');
                     echo $this->Form->control('effectue');
                 ?>
